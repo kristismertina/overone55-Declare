39170828e344c18fa1995d93829ac4b5873f3b94
kris <kristismertina@gmail.com>
2021-05-26 23:51:54 +0300

    first_commit

---
+++ new_declare.py
@@ -0,0 +1,31 @@
+import datetime
+class Declare:
+    def __init__ (self, heading, descip, aurhor, looking):
+        self.__heading = heading
+        self.__descip = descip
+        self.__aurhor = aurhor
+        self.__datetime = datetime.datetime.today()
+        self.__looking = looking
+
+    def __str__ (self):
+        return f"{self.__heading}, {self.__descip}, {self.__aurhor}, {self.__datetime} , {self.__looking}"
+
+    def looking (self):
+        return self
+        declare.count +=1
+
+
+    def change_heading (self, name):
+        self.__heading = name
+        return self.__heading
+
+    def change_descip (self, name):
+        self.__descip = name
+        return self.__descip
+
+
+
+a = Declare("Animals", "cat dog fish elefant", "Paul Down", 0)
+#print(a.change_descip("dffddf"))
+#print(a.change_heading("hjhhhhjnnnnn"))
+print (a.looking())
